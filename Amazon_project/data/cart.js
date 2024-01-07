1994f12be776736a7086460fe9dcec7818548a44
santhiraju <santhirajumerla@gmail.com>
2024-01-07 10:38:23 +0530

    updated checkout page and modified delivery oprtions code(radio buttons) used modules for currency formatting

--- Amazon_project/data/cart.js
+++ Amazon_project/data/cart.js
@@ -1,4 +1,13 @@
-export const cart = [];
+export const cart = [
+  {
+    productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity:2,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity:3,
+  }
+];
 export function addToCart(productId)
 {
   let matchingItem;
@@ -9,7 +18,7 @@ export function addToCart(productId)
   cart.forEach((item) => {
     if (productId === item.productId) {
       matchingItem = item;
-    }
+    };
   });
 
   if (matchingItem) {
@@ -20,4 +29,4 @@ export function addToCart(productId)
       quantity: quantity
     });
   }
-}
+};
